23cd9942b9b3b2c5df82761a7bc0f6c6b7ab1b45
V-V-V-V-V-V <113368119+V-V-V-V-V-V@users.noreply.github.com>
2023-02-15 23:48:46 +0530

    Add files via upload
    idk im just trying

---
+++ src/components/Footer.js
@@ -0,0 +1,24 @@
+export default function Footer() {
+    return(
+        <footer className="footer border-t-2 border-gray-300 pt-5">
+        <ul className="flex flex-wrap items-center justify-center">
+         <li><strong>Your name:</strong>vishwesg
+         </li>
+         <li><strong>email:</strong>mymail@asdfg.com
+         </li>
+         <li><strong>ph number:</strong>123456789
+         </li>
+         <li><strong>Bank:</strong>vish bank
+         </li>
+         <li><strong>Account holder:</strong>Meeeeeeeeeees
+         </li>
+         <li><strong>Account number:</strong>123456789
+         </li>
+         <li><strong>website:</strong>whatever.com
+         </li>
+        </ul>
+     
+     
+         </footer>
+    )
+}
---
+++ src/components/Some.js
@@ -0,0 +1,37 @@
+import React from "react"
+export default function Some({description,quantity,amount,price,list,setList}) {
+    return(
+       <>
+        <table width="100%">
+       <thead>
+                    <tr className="bg-gray-100 p-1">
+                        <td className="font-bold">Item description</td>
+                        <td className="font-bold">Quantity</td>
+                        <td className="font-bold">Price</td>
+                        <td className="font-bold">Amount</td>
+                    </tr>
+
+                </thead>
+        {list.map(({id,description,quantity,price,amount})=>(
+            <>
+                
+            <React.Fragment key={id}>
+            
+                <tbody>
+                    <tr>
+                        <td>{description}</td>
+                        <td>{quantity}</td>
+                        <td>{price}</td>
+
+                        <td>{amount}</td>
+
+
+
+                    </tr>
+                </tbody>
+                </React.Fragment></>
+        ))}
+        </table>
+       </>
+    )
+}
