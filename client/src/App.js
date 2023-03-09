0fe6e71327d35dbc66ed0cd0ed0731fd87c664b1
Akshay-3114 <akshaysuresh492@gmail.com>
2023-03-09 10:51:25 +0530

    mui

--- client/src/App.js
+++ client/src/App.js
@@ -4,10 +4,23 @@ import Home from "./pages/Home/Home";
 import Products from './pages/Products/Products';
 import AddProduct from './pages/AddProduct/AddProduct';
 import EditProduct from './pages/EditProduct/EditProduct';
+import SidebarApp from  './pages/global/SidebarApp';
+import Topbar from './pages/global/Topbar';
+import { ColorModeContext, useMode } from './theme';
+import { CssBaseline, ThemeProvider } from '@mui/material';
+
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
+    <ColorModeContext.Provider value={colorMode}>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+       <div className='App' >
+         <SidebarApp />
+          <main className='content'>
+            <Topbar />
+
       <Routes>
         <Route path='/' element={<Home />} />
         <Route path="/login" element={<Login />} />          
@@ -15,7 +28,10 @@ function App() {
         <Route path="/products/add" element={<AddProduct />} />          
         <Route path="/products/:productId/edit" element={<EditProduct />} />          
       </Routes>
+      </main>
     </div>
+    </ThemeProvider>
+  </ColorModeContext.Provider>
   );
 }
 
