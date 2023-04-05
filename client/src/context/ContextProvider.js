81166624bb3296676890f3a61e9c6913a6f9cdde
Akshay-3114 <akshaysuresh492@gmail.com>
2023-04-05 22:55:43 +0530

    Created product index page

--- client/src/context/ContextProvider.js
+++ client/src/context/ContextProvider.js
@@ -6,13 +6,17 @@ const AppContext = createContext(null);
 
 // returns the context provider.
 const AppContextProvider = ({ children }) => {
-   const [customerData , setCustomerData] = useState([])
+   const [customerData , setCustomerData] = useState([]);
+   const [productData, setProductData] = useState([]);
    const addCustomerData = (customer)=>{
       setCustomerData([...customerData,customer]);
    }
+   const addProductData = (product) =>{
+      setProductData([...productData,product]);
+   }
    return (
       // when this context is used, the value returned would be array of 2 size -> [state, dispatch]
-      <AppContext.Provider value={{state: useReducer(reducer, initialState),customerData,setCustomerData,addCustomerData}}>
+      <AppContext.Provider value={{state: useReducer(reducer, initialState),customerData,setCustomerData,addCustomerData,productData,setProductData,addProductData}}>
          {children}
       </AppContext.Provider>
    )
